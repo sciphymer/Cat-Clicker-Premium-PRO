0564b04ab67a1da280d2a0d0f53a8644d972fdbe
Vincent Ng <root@Vincent-PC.localdomain>
2018-07-24 22:32:59 +0800

    init commit, stuck by the problem of global and local variable definition

---
+++ js/app.js
@@ -0,0 +1,189 @@
+
+/* ======= Model ======= */
+
+var model = {
+    currentCat: null,
+    cats: [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+        }
+    ]
+};
+
+
+/* ======= Octopus ======= */
+
+var octopus = {
+
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
+
+        // tell our views to initialize
+        catListView.init();
+        catView.init();
+        adminView.init();
+    },
+
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+    incrementCounter: function() {
+        model.currentCat.clickCount++;
+        catView.render();
+    }
+};
+
+
+/* ======= View ======= */
+
+var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // on click, increment the current cat's counter
+        this.catImageElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgSrc;
+    }
+};
+
+var catListView = {
+
+    init: function() {
+        // store the DOM element for easy access later
+        this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        var cat, elem, i;
+        // get the cats we'll be rendering from the octopus
+        var cats = octopus.getCats();
+
+        // empty the cat list
+        this.catListElem.innerHTML = '';
+
+        // loop over the cats
+        for (i = 0; i < cats.length; i++) {
+            // this is the cat we're currently looping over
+            cat = cats[i];
+
+            // make a new cat list item and set its text
+            elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
+            elem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                };
+            })(cat));
+
+            // finally, add the element to the list
+            this.catListElem.appendChild(elem);
+        }
+    }
+};
+
+var adminView = {
+
+    init: function() {
+        this.adminMenu = document.getElementById("admin-area");
+        this.adminForm = this.adminMenu.childNodes[3];
+        this.adminForm.style.visibility="hidden";
+        this.name_field = document.querySelectorAll("[type=text]")[0];
+        this.ImgURL_field = document.querySelectorAll("[type=text]")[1];
+        this.Click_field = document.querySelectorAll("[type=text]")[2];
+        this.adminBtn = document.getElementById("admin-btn");
+        this.saveBtn = document.getElementById("save-btn");
+        this.cancelBtn = document.getElementById("cancel-btn");
+        this.render();
+    },
+    render: function(){
+        this.adminBtn.addEventListener('click', function(){
+            // why do the adminMenu and adminForm need redefined?
+            // aren't they defined in init before?
+            // ##################################################
+            // this.adminMenu = document.getElementById("admin-area");
+            // this.adminForm = this.adminMenu.childNodes[3];
+            // ##################################################
+            if(this.adminForm.style.visibility=="hidden"){
+                this.adminForm.style.visibility="visible";
+                let currentCat = octopus.getCurrentCat();
+                this.name_field.textContent = currentCat.name;
+                this.ImgURL_field.textContent = currentCat.imgSrc;
+                this.Click_field.textContent = currentCat.clickCount;
+                saveBtn.addEventListener('click', function(){
+
+                });
+            }
+            else
+                this.adminForm.style.visibility="hidden";
+
+
+        });
+    }
+};
+
+// make it go!
+octopus.init();
